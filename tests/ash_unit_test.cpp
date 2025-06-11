81b61c1f5f3fbcf1f5b5a3d793ef0bf1df3950e9
zhangziyi10 <zhangziyi10@xiaomi.com>
2025-06-11 14:55:57 +0800

    add unit testcase for ash fds/scoped_fd.cpp
    Signed-off-by: zhangziyi10 <zhangziyi10@xiaomi.com>

--- tests/ash_unit_test.cpp
+++ tests/ash_unit_test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
-#include "ash/device_info/device_info.h"
 #include "device_info/device_info_test.cpp"
+#include "fds/scoped_fd_test.cpp"
+
 extern "C" int main(int argc, char** argv) {
-  ::testing::GTEST_FLAG(filter) = "DeviceInfo*";
+  ::testing::GTEST_FLAG(filter) = "DeviceInfo*:ScopedFD*";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
