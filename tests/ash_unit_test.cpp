9eef61c657f70fb7f9d616e00079d7998c7fa324
zhangziyi10 <zhangziyi10@xiaomi.com>
2025-06-16 15:47:13 +0800

    add unittest for ash/file/file_path_test.cpp
    Signed-off-by: zhangziyi10 <zhangziyi10@xiaomi.com>

--- tests/ash_unit_test.cpp
+++ tests/ash_unit_test.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include "device_info/device_info_test.cpp"
 #include "fds/scoped_fd_test.cpp"
+#include "file/file_path_test.cpp"
 
 extern "C" int main(int argc, char** argv) {
-  ::testing::GTEST_FLAG(filter) = "DeviceInfo*:ScopedFD*";
+  ::testing::GTEST_FLAG(filter) = "DeviceInfo*:ScopedFD*:FilePath*";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
