9858a9905816ddeba95a65343022de11010b7110
zhangziyi10 <zhangziyi10@xiaomi.com>
2025-06-18 09:44:00 +0800

    add unittest for ash/time/time.cpp
    Signed-off-by: zhangziyi10 <zhangziyi10@xiaomi.com>

--- tests/ash_unit_test.cpp
+++ tests/ash_unit_test.cpp
@@ -3,9 +3,10 @@
 #include "fds/scoped_fd_test.cpp"
 #include "file/file_path_test.cpp"
 #include "time/duration_test.cpp"
+#include "time/time_test.cpp"
 
 extern "C" int main(int argc, char** argv) {
-  ::testing::GTEST_FLAG(filter) = "DeviceInfo*:ScopedFD*:FilePath*:Duration*";
+  ::testing::GTEST_FLAG(filter) = "DeviceInfo*:ScopedFD*:FilePath*:Duration*:Time*";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
