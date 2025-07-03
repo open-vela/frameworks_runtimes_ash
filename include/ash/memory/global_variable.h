983ddfb8c59b3ea93af0fefc9411d4ea3aa2bb85
gaoyuan28 <gaoyuan28@xiaomi.com>
2025-07-03 19:34:56 +0800

    GlobalVariable: correct type of offset_
    Signed-off-by: gaoyuan28 <gaoyuan28@xiaomi.com>

--- include/ash/memory/global_variable.h
+++ include/ash/memory/global_variable.h
@@ -109,7 +109,7 @@ class GlobalVariable {
   }
 
  private:
-  T offset_;
+  int offset_;
   ASH_DISALLOW_COPY_AND_MOVE(GlobalVariable);
 };
 
@@ -127,7 +127,7 @@ class GlobalVariableWithInitializer {
   }
 
  private:
-  T offset_;
+  int offset_;
   ASH_DISALLOW_COPY_AND_MOVE(GlobalVariableWithInitializer);
 };
 
