c2b89f053b788c5b5a56f794af9b3eb959a8f0e0
zhangziyi10 <zhangziyi10@xiaomi.com>
2025-07-16 20:54:17 +0800

    add unittest for ash/memory/variable_segment_test.cpp
    Signed-off-by: zhangziyi10 <zhangziyi10@xiaomi.com>

--- tests/ash_unit_test.cpp
+++ tests/ash_unit_test.cpp
@@ -3,16 +3,17 @@
 #include "fds/scoped_fd_test.cpp"
 #include "file/file_path_test.cpp"
 #include "file/file_test.cpp"
-#include "time/duration_test.cpp"
-#include "time/time_test.cpp"
 #include "memory/raw_ptr_test.cpp"
+#include "memory/variable_segment_test.cpp"
 #include "strings/number_string_conversions_test.cpp"
 #include "strings/string_converter_test.cpp"
+#include "time/duration_test.cpp"
+#include "time/time_test.cpp"
 
 extern "C" int main(int argc, char** argv) {
   ::testing::GTEST_FLAG(filter) =
       "DeviceInfo*:ScopedFD*:File*:FilePath*:Duration*:Time*:RawPtr*:"
-      "NumberStringConversion*:StringConverter*";
+      "NumberStringConversion*:StringConverter*:VariableSegment*";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
