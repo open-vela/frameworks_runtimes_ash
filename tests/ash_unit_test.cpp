9b830eb676b138534d499e2d0965cfd4e1cc682b
zhangziyi10 <zhangziyi10@xiaomi.com>
2025-07-17 16:48:38 +0800

    add unittest for ash/functional
    Signed-off-by: zhangziyi10 <zhangziyi10@xiaomi.com>

--- tests/ash_unit_test.cpp
+++ tests/ash_unit_test.cpp
@@ -3,6 +3,8 @@
 #include "fds/scoped_fd_test.cpp"
 #include "file/file_path_test.cpp"
 #include "file/file_test.cpp"
+#include "functional/closure_test.cpp"
+#include "functional/fn_once_test.cpp"
 #include "memory/raw_ptr_test.cpp"
 #include "memory/variable_segment_test.cpp"
 #include "strings/number_string_conversions_test.cpp"
@@ -13,7 +15,8 @@
 extern "C" int main(int argc, char** argv) {
   ::testing::GTEST_FLAG(filter) =
       "DeviceInfo*:ScopedFD*:File*:FilePath*:Duration*:Time*:RawPtr*:"
-      "NumberStringConversion*:StringConverter*:VariableSegment*";
+      "NumberStringConversion*:StringConverter*:VariableSegment*:FnOnce*:"
+      "*Closure*";
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
